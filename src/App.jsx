573bc20d0e9aedf8cb58032a0a027a50d8e5e367
Ian De Jesus <ian.dejesus.fontilea@gmail.com>
2023-12-16 00:04:53 -0800

    testing

--- src/App.jsx
+++ src/App.jsx
@@ -30,7 +30,7 @@ function App() {
   }, []);
 
   let [open, openState] = useState(false);
-  console.log("This is a menu:" + open)
+  console.log("This is a menu: does this work?" + open)
   
   return (
     <div className=' bg-gradient-to-br from-orange-800 from-10% v	to-sky-950  to-90% min-h-screen w-full'>
