ac9ffbbcad216262d458f69e64082b4228689038
Ian De Jesus <ian.dejesus.fontilea@gmail.com>
2024-01-06 02:21:50 -0800

    adding data to cards

--- src/strava_app/pages/Activity.jsx
+++ src/strava_app/pages/Activity.jsx
@@ -17,18 +17,26 @@ function StravaActivity() {
 
   return (
     <>
-      <div className="flex p-2">
-        <img
-          className="rounded-full w-32 "
-          src="https://i.pravatar.cc/150"
-          alt="avatar"
-        />
+      <div className="w-full flex p-2 justify-center ">
+      <img
+        className="rounded-full w-32 "
+        src="https://i.pravatar.cc/150"
+        alt="avatar"
+      />
       </div>
-      <div className=" ">
+      <div className="w-full justify-center items-center ">
         <h1 className="text-1xl font-bold text-left text-white ">
-          My Total Activities
+          My Current Activity
         </h1>
-        {activityData && <div>{activityData.resource_state}</div>}
+        {activityData && (
+          <>
+            <div className=" flex justify-between md:justify-around">
+              <p className="text-white font-bold"> Distance:</p>
+              <p className="text-white">{(activityData.distance*0.000621371192).toFixed(2)}Mi</p>
+            </div>
+            <div className="text-white">Calories: {activityData.calories}</div>
+          </>
+        )}
       </div>
     </>
   );
@@ -48,8 +56,8 @@ function Activity() {
         {/* Card One - Activity Profile */}
         <div className="flex  flex-wrap gap-2 md:gap-5  md:flex-nowrap  ">
           <div className="container w-100 md:flex-auto md:w-40 ">
-            <div className="border-slate-100/10 border-2 rounded-xl  backdrop-blur-md shadow-xl opacity-1  relative bg-transparent  animate-fadeInAndUp ">
-              <div className="flex p-2">
+            <div className="border-slate-100/0 border-2 rounded-xl  backdrop-blur-xl shadow-2xl opacity-1  relative bg-transparent  animate-fadeInAndUp ">
+              <div className="flex p-2 w-full  flex-col md:flex-row justify-center items-center">
                 <StravaActivity />
               </div>
             </div>
@@ -60,11 +68,11 @@ function Activity() {
             <div className="border-slate-100/10 border-2 rounded-xl  backdrop-blur-md shadow-xl opacity-1  relative bg-transparent  animate-fadeInAndUp ">
               <div className="flex p-2">
                 <div className="flex p-2">
-                  <img
+                  {/* <img
                     className="rounded-full w-32 "
                     src="https://i.pravatar.cc/150"
                     alt="avatar"
-                  />
+                  /> */}
                 </div>
                 <div className=" ">
                   <h1 className="text-1xl font-bold text-left text-white ">
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,14 +9,19 @@ module.exports = {
       keyframes: {
         fadeInAndUp: {
           '0%': { opacity: '0', transform: 'translateY(3rem)' },
-          '65%': { opacity: '0.85', transform: 'translateY(-0.15rem)' },
+          '85%': { opacity: '0.85', transform: 'translateY(-0.15rem)' },
           '100%': { opacity: '1', transform: 'translateY(0)' },
         }
       },
       animation: {
         fadeInAndUp: 'fadeInAndUp 1s ease-in-out forwards'
+      },
+      boxShadow: {
+        '3xl': '0 35px 60px -15px rgba(0, 0, 0, 0.3)',
       }
+      
     },
+    
   },
   plugins: [ 
     require('@tailwindcss/typography'),
